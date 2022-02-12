581cf6f9c3dc23f0da4da3ad56806c46bbe8c7a9
Marcelo Pinheiro Montanher <marcelo.montanher14@gmail.com>
2022-02-12 09:36:42 -0300

    Google fonts import

--- lib/class_1/card_screen.dart
+++ lib/class_1/card_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CardScreen extends StatelessWidget {
   const CardScreen({Key? key}) : super(key: key);
@@ -9,10 +10,18 @@ class CardScreen extends StatelessWidget {
       backgroundColor: Colors.teal,
       body: SafeArea(
         child: Column(
-          children: const [
-            CircleAvatar(
+          children: [
+            const CircleAvatar(
               radius: 50,
               backgroundImage: AssetImage('assets/images/profile.jpg'),
+            ),
+            Text(
+              'Marcelo Montanher',
+              style: GoogleFonts.indieFlower(
+                fontSize: 26,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ],
         ),
